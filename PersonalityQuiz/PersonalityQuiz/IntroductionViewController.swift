4e5f3cf77d62c48227af12c19d45930e66a69e31
kengo-taka <1117marine1117@gmail.com>
2020-12-18 21:38:17 -0800

    first commit

---
+++ PersonalityQuiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Takamiya Kengo on 2020/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func unwindToQuizIntroduction(segue:
+    UIStoryboardSegue) {
+    }
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
